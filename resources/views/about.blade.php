c202222bbbcf2afd8d9fa25bec2eb6ad3a383946
Suraj Verma <surajkumar8707>
2025-02-12 22:20:13 +0530

    new changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,7 +8,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="banner-heading">
-                            <h1 class="banner-title">Privacy Policy</h1>
+                            <h1 class="banner-title">About Page</h1>
                         </div>
                     </div>
                 </div>
